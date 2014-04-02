1c08ae0bb91b66f16712c2097b4444d07a16643a
John Otander <johnotander@gmail.com>
2014-04-01 22:47:55 -0600

    Add more data to wrapper classes.

--- lib/nest_wrapper/device.rb
+++ lib/nest_wrapper/device.rb
@@ -1,9 +1,13 @@
 module NestWrapper
   class Device
-    attr_accessor :name
+    attr_accessor :name, :zip, :country. :state
 
     def initialize(status)
-      self.name = NestWrapper.nest.status['structure'][NestWrapper.nest.status['structure'].keys.first]['name']
+      self.name         = NestWrapper.nest.status['structure'][NestWrapper.nest.status['structure'].keys.first]['name']
+      self.zip          = NestWrapper.nest.status['structure'][NestWrapper.nest.status['structure'].keys.first]['postal_code']
+      self.country_code = NestWrapper.nest.status['structure'][NestWrapper.nest.status['structure'].keys.first]['country_code']
+
+      self.state = NestWrapper::State.new(status)
     end
   end
 end
--- lib/nest_wrapper/state.rb
+++ lib/nest_wrapper/state.rb
@@ -1,9 +1,10 @@
 module NestWrapper
   class State
-    attr_accessor :temperature, :leaf, :online
+    attr_accessor :temperature, :leaf, :online, :away
 
     def initialize(status)
       self.online = NestWrapper.nest.status['track'].to_a.flatten[1]['online']
+      self.away   = NestWrapper.nest.status['structure'][NestWrapper.nest.status['structure'].keys.first]['away']
     end
   end
 end
