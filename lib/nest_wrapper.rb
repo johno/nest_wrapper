8f14d5c18031c2f5a4f90023ea185773d1620c4b
John Otander <johnotander@gmail.com>
2014-04-15 21:57:09 -0600

    Add more config data.

--- lib/nest_wrapper.rb
+++ lib/nest_wrapper.rb
@@ -2,15 +2,20 @@ require 'nest_thermostat'
 
 require 'nest_wrapper/version'
 require 'nest_wrapper/device'
+require 'nest_wrapper/state'
 
 module NestWrapper
   
   class << self
-    attr_accessor :nest, :status
+    attr_accessor :nest, :status, :device
   end
 
   def self.login(email, password)
     self.nest = NestThermostat::Nest.new({ email: email, password: password })
+
+    set_status
+
+    self.device = NestWrapper::Device.new(status)
   end
 
   def self.set_status
--- lib/nest_wrapper/device.rb
+++ lib/nest_wrapper/device.rb
@@ -1,11 +1,12 @@
 module NestWrapper
   class Device
-    attr_accessor :name, :zip, :country, :state
+    attr_accessor :name, :zip, :country, :state, :config_data
 
     def initialize(status)
-      self.name         = NestWrapper.nest.status['structure'][NestWrapper.nest.status['structure'].keys.first]['name']
-      self.zip          = NestWrapper.nest.status['structure'][NestWrapper.nest.status['structure'].keys.first]['postal_code']
-      self.country_code = NestWrapper.nest.status['structure'][NestWrapper.nest.status['structure'].keys.first]['country_code']
+      self.name        = NestWrapper.nest.status['structure'][NestWrapper.nest.status['structure'].keys.first]['name']
+      self.zip         = NestWrapper.nest.status['structure'][NestWrapper.nest.status['structure'].keys.first]['postal_code']
+      self.country     = NestWrapper.nest.status['structure'][NestWrapper.nest.status['structure'].keys.first]['country_code']
+      self.config_data = NestWrapper.nest.status['device'][NestWrapper.nest.status['device'].keys.first]
 
       self.state = NestWrapper::State.new(status)
     end
